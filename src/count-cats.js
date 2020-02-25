c0759e74e8025cdca659a72a58e7b87bf2f11ee6
Tsimafei Karpeyeu <tkarpeyeu@gmail.com>
2020-02-25 21:39:50 +0300

    feat: add count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,16 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(input) {
+  let result = 0;
+  const cat = '^^';
+  for (let i = 0; i < input.length; i++) {
+    // console.log(`external array`, input[i]);
+    for (let j = 0; j < input[i].length; j++) {
+      // console.log('internal', input[i][j]);
+      if (cat === input[i][j]) {
+        result += 1;
+      }
+    }
+
+  }
+
+  return result;
 };
